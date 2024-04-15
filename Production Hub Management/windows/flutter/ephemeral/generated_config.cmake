358355138576d7e39a58982aff69a3817eb3615b
Cyril Thomas <122374383+Cyrilthomas123@users.noreply.github.com>
2024-04-15 12:15:51 +0530

    Add files via upload

---
+++ Production Hub Management/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\MCA\\Internship\\PHM\\login" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.105.216+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 105 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 216 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=F:\\MCA\\Internship\\PHM\\login"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\MCA\\Internship\\PHM\\login\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\MCA\\Internship\\PHM\\login"
+  "FLUTTER_TARGET=F:\\MCA\\Internship\\PHM\\login\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\MCA\\Internship\\PHM\\login\\.dart_tool\\package_config.json"
+)
